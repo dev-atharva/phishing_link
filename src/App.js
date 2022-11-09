2f1759fff8d97dd128e6e4633ca1b7720ac3f933
Atharva Mahadeokar <athaarva999@gmail.com>
2022-11-09 20:06:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [link,setLink]=useState("")
+  const [res,setRes]=useState("")
+
+  const handleSubmit= (e)=>{
+    e.preventDefault();
+    const submi={"body":link}
+    axios.post('http://127.0.0.1:5000/predict',submi)
+    .then(
+      response=>{setRes(response.data['result'],
+      alert(response.data['result']))
+    setLink("")}
+    )
+    .catch(
+      error=>{alert(error)}
+    )
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='myele'>
+        <div className='Heading'>
+          Phishing Link Detection 
+        </div>
+        <form className='myform' >
+          <input type='text' placeholder='Enter the link' value={link} onChange={(e)=>{setLink(e.target.value)}}></input>
+          <button onClick={handleSubmit}> Submit</button>
+        </form>
+        <div className='res'>
+          {res}
+        </div>
+      </div>
     </div>
   );
 }
